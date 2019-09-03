dd6e4274b554bc98a051855dbdcdecd47fe27173
Ariel Shaqed (Scolnicov) <ariels@shiftjs.com>
2019-09-03 15:58:54 +0300

    CR: Clean up toString, use t.is instead of t.assert for scalars

--- src/test/shell.test.ts
+++ src/test/shell.test.ts
@@ -7,36 +7,36 @@ const process = evolve({ out: (x) => x.toString(), err: (x) => x.toString() });
 
 test('shell returns standard outputs', async (t) => {
   const shell = new Shell();
-  const { out, err } = process(await shell.run('echo foo\necho -n bar\n'));
-  t.assert(out.toString() === 'foo\nbar');
-  t.assert(err.toString() === '');
-  const { out: out2, err: err2 } = process(await shell.run('echo quux'));
-  t.assert(out2.toString() === 'quux\n');
-  t.assert(err2.toString() === '');
+  const ret = process(await shell.run('echo foo\necho -n bar\n'));
+  t.is(ret.out, 'foo\nbar');
+  t.is(ret.err, '');
+  const ret2 = process(await shell.run('echo quux'));
+  t.is(ret2.out, 'quux\n');
+  t.is(ret2.err, '');
 });
 
 test('shell returns standard outputs with incomplete lines', async (t) => {
   const shell = new Shell();
-  const { out, err } = process(await shell.run('echo -n foo'));
-  t.assert(out.toString() === 'foo');
-  t.assert(err.toString() === '');
+  const ret = process(await shell.run('echo -n foo'));
+  t.is(ret.out, 'foo');
+  t.is(ret.err, '');
 });
 
 test('shell returns standard errors', async (t) => {
   const shell = new Shell();
   const { out, err } = process(await shell.run('1>&2 echo foo; 1>&2 echo -n bar\n'));
-  t.assert(err.toString() === 'foo\nbar');
-  t.assert(out.toString() === '');
+  t.is(err, 'foo\nbar');
+  t.is(out, '');
   const { out: out2, err: err2 } = process(await shell.run('1>&2 echo quux\n'));
-  t.assert(err2.toString() === 'quux\n');
-  t.assert(out2.toString() === '');
+  t.is(err2, 'quux\n');
+  t.is(out2, '');
 });
 
 test('shell returns both together', async (t) => {
   const shell = new Shell();
   const { out, err } = process(await shell.run('echo foo; 1>&2 echo bar'));
-  t.assert(out.toString() === 'foo\n');
-  t.assert(err.toString() === 'bar\n');
+  t.is(out, 'foo\n');
+  t.is(err, 'bar\n');
 });
 
 const exitSignal = pick(['exitCode', 'signal']);
@@ -56,11 +56,11 @@ test('shell reports signal', async (t) => {
 test('shell raises exception when its shell exits', async (t) => {
   const shell = new Shell();
   const error: ShellError = await t.throwsAsync(shell.run('exit 17'), ShellError);
-  t.assert(error.exitCode === 17);
+  t.is(error.exitCode, 17);
 });
 
 test('shell raises exception when its shell dies', async (t) => {
   const shell = new Shell();
   const error: ShellError = await t.throwsAsync(shell.run('kill -TERM $$'), ShellError);
-  t.assert(error.signal === 'SIGTERM');
+  t.is(error.signal, 'SIGTERM');
 });
