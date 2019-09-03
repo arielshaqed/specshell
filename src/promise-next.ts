bada7b95f496b3f7860f389ea82ed6cce9f00db8
Ariel Shaqed (Scolnicov) <ariels@shiftjs.com>
2019-09-03 14:57:54 +0300

    CR: Use defer-promise instead of home-grown Mailbox
    Mailbox is a bit nicer because it is a `Promise`, but OTOH
    defer-promise is existing code.

--- src/promise-next.ts
+++ src/promise-next.ts
@@ -1,24 +1,7 @@
-export class Mailbox<T> extends Promise<T> {
-  private res: (t: T) => void;
-
-  // Extending Promise<T> requires passing in a function to get an
-  // executor, you cannot just invent it in the constructor.
-  // Weirdnesses related to
-  // https://github.com/nodejs/node/issues/13678.
-  constructor(func: (res: () => void, _rej: () => any) => void = () => 1) {
-    let res: () => void;
-    super((s, j) => {
-      res = s;
-      func(s, j);
-    });
-    this.res = res!;
-  }
-
-  public resolve(t: T) { this.res(t); }
-}
+import defer = require('defer-promise');
 
 export class PromiseNext<T> {
-  protected query: Array<Mailbox<T>> = [];
+  protected query: Array<DeferPromise.Deferred<T>> = [];
   protected queue: T[] = [];
 
   public push(t: T) {
@@ -28,8 +11,8 @@ export class PromiseNext<T> {
 
   public async next(): Promise<T> {
     if (this.queue.length > 0) return this.queue.shift()!;
-    const wait = new Mailbox<T>();
+    const wait = defer<T>();
     this.query.push(wait);
-    return await wait;
+    return await wait.promise;
   }
 }
